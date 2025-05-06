439aba92aeeea1b74d2da451ea9ab9441ac600e9
sandy424 <sandy424@naver.com>
2025-05-06 22:20:29 +0900

    [add/delete]

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,8 +3,9 @@ import '../style/Style.css';
 
 function Home() {
     return(
-        <div className="main">
-            <h2>Welcome to your page!</h2>
+        <div id="profile-view" className="container" >
+            <div id="profile" className="title">PROFILE</div>
+            
         </div>
     );
 }
