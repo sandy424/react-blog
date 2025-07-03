333ccd6e769384a19777c82e97328c612e972090
DESKTOP-Q5CFOMV\user <sandy424@naver.com>
2025-07-03 23:03:14 +0900

    [update] 홈화면 수정

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,12 +1,36 @@
 import React from "react";
 import '../style/Style.css';
+import search from '../../img/search button.png';
 
 function Home() {
     return(
-        <div id="profile-view" className="container" >
-            <div className="title">PROFILE</div>
-            
-        </div>
+        <nav className="nav">
+            <div className="top-wrapper">
+                <h2>오늘의 Top</h2>
+
+                <div className="boxs">
+                    <div className="feed-box">
+                        {/* <div className="thumb"></div> */}
+                        <p><strong>title</strong><br/>내용</p>
+                    </div>
+                    <div className="feed-box">
+                        <div className="thumb"></div>
+                        <p><strong>title</strong><br/>내용</p>
+                    </div>
+                    <div className="feed-box">
+                        <div className="thumb"></div>
+                        <p><strong>title</strong><br/>내용</p>
+                    </div>
+                
+                    <div className="search-bar">
+                        <input type="text" placeholder="search"/>
+                        <button className="search-btn">
+                            <img src={search}/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 }
 
